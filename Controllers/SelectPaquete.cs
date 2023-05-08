3d015d1147e16121b13668b3b2131d3fd417a8db
URIEL GREGORIO CWIRENBAUM <47866146@comunidadort.edu.ar>
2023-05-08 08:57:56 -0300

    eso stanca

---
+++ Controllers/SelectPaquete.cs
@@ -0,0 +1,3 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TP4_Paquetes_Turisticos.Controllers;
--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -2,15 +2,50 @@ namespace TP4_Paquetes_Turisticos.Models;
 
 public class ORTWorld
 {
-    public static List<string> ListaDestinos {get; private set;} = new List<string>();
-    public static List<string> ListaHoteles {get; private set;}
-    public static List<string> ListaAereos {get; private set;}
-    public static List<string> ListaExcursiones {get; private set;}
-    public static Dictionary<string, Paquete> Paquetes {get; private set;}
+    public static List<string> ListaDestinos {get; private set;} = new List<string>
+    {
+        "buenos aires", "paris", "barcelona",  "canada",  "florida"/*🦅🦅🦅*/,  "polonia",  "lituania",  "senegal",  "grieta del invocador",  "runaterra"
+    };
+    public static List<string> ListaHoteles {get; private set;} = new List<string>
+    {
+        "trivago", "santa maria", "babilonia", "stanca", "bikel", "momazos", "grasa", "notengoideadequeponer", "goku", "kratos" 
+    };
+    public static List<string> ListaAereos {get; private set;} = new List<string>
+    {
+        "stanca", "binker", "iberia", "poppy"/*Aerolineas Martillo*/, "diegollanos", "shebar", "readme.md", "aereo", "aprobanosporfa", "quemalmomo", 
+    };
+    public static List<string> ListaExcursiones {get; private set;} = new List<string>
+    {
+        "esqui", "entrenamiento", "calistenia", "tirolesa", "tracking", "silent-fest", "rafting", "canopy", "tutorial-c#", "tutorial-pseint", 
+    };
+// no es necesario un tutorial c# si tenemos a binker
+//     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⢴⣮⣽⣿⣿⣿⣿⣿⣯⣭⣭⣿⣢⢄⡀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣾⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⢆⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⡿⠛⠋⠙⣉⠛⣛⣿⣿⣿⠟⠛⢧⢷⠀⠀
+// ⠀⠀⠀⠀⠀⠀⡼⣿⣿⣿⣿⣿⣿⠯⠄⠀⠀⠀⠀⣦⣤⣽⣿⣟⣗⣄⠈⢣⡗⠀
+// ⠀⠀⠀⠀⠀⢠⢿⣿⣿⣿⣿⣿⣿⡴⠚⠉⠀⢀⣤⣬⣬⣿⣿⣿⠹⣿⡇⠀⣿⠀
+// ⠀⠀⠀⠀⠀⢸⢸⣿⣿⣿⣿⣿⠋⠀⠀⢠⠴⠟⣛⣿⣿⣿⣿⣿⣶⣾⣰⡀⢹⡢
+// ⠀⠀⠀⠀⠀⣸⢾⠟⠻⣿⣿⠇⠀⠀⠀⠐⢿⣿⣿⣿⣿⣿⣿⡟⢻⢻⣿⣿⣶⡇
+// ⠀⠀⠀⠀⢀⣾⣏⣐⡄⠀⣯⡀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⠄⠘⣿⣿⣿⣷⡅
+// ⠀⠀⠀⠀⢸⣤⣿⣿⠀⠀⣿⣷⡀⠀⠀⠀⣠⣶⣿⣿⣿⣿⠇⣄⣀⠸⡾⣷⡄⡇
+// ⠀⠀⠀⠀⠈⠣⣃⡈⢉⣸⣿⡻⣿⣮⣴⣾⡏⢀⣽⣿⣿⣿⣶⣶⣶⣴⣇⣿⠀⣱
+// ⠀⠀⠀⠀⠀⠀⡏⡏⠁⣿⢿⣆⣿⣿⣿⣿⣧⣿⣿⣿⣛⣿⣿⣿⣿⡦⣾⡟⢠⣃
+// ⠀⠀⠀⠀⠀⠀⣧⡇⢠⡏⢂⢹⣿⣿⣿⣿⣿⣿⣿⣿⡷⣬⣭⣙⡛⢳⣼⣿⣿⣎
+// ⠀⠀⠀⠀⠀⢠⢿⠀⠘⣿⣧⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⣿⣿⣥⣿⣿⢿⡿
+// ⠀⠀⠀⠀⠀⢸⡟⠀⠀⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣸⣿⢻⡿⠀
+// ⠀⠀⠀⠀⠀⣯⡇⠀⠀⠀⠀⠈⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⢸⠁⠀
+// ⠀⠀⠀⢀⣴⠟⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⣜⡆⠀
+// ⣒⠶⡛⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⡿⣠⡟⠀⠀
+    public static Dictionary<string, Paquete> Paquetes {get; private set;} = new Dictionary<string, Paquete>();
 
     public bool IngresarPaquete(string destinoSeleccionado, Paquete paquete) 
     {
         bool retorno = false;
+        foreach (string item in ListaDestinos)
+        {
+            if(item == destinoSeleccionado) retorno = true;
+        }
+        if(retorno) Paquetes.Add(destinoSeleccionado, paquete);
         return retorno;
     }
 }
