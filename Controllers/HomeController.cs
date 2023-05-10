0c2b6390d9a13d038f508af729936de67a39206a
URIEL GREGORIO CWIRENBAUM <47866146@comunidadort.edu.ar>
2023-05-10 08:53:23 -0300

    casa

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,9 +18,12 @@ public class HomeController : Controller
         return View();
     }
     public IActionResult GuardarPaquete(string Destino, string Hotel, string Aereo, string Excursion)
-    {            
+    {
+        bool error = true;
+        if(Destino == null || Hotel == null || Aereo == null || Excursion == null) error = false;
         Paquete nuevoPaquete = new Paquete(Hotel, Aereo, Excursion);
         ORTWorld.Paquetes.Add(Destino, nuevoPaquete);
+        ViewBag.Error = error;  
         return View();
     }
     //ESTE ES EL UNICO CONTROLLER, CUALQUIER COSA DE CONTROLLER VA ACA Y NO SE CAMBIA
