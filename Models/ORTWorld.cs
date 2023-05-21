36ce0137fad03e89628a7fd6e85c9c4327438cce
unknown <benjafloo2007@gmail.com>
2023-05-21 16:44:55 -0300

    IMAGENES

--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -4,15 +4,15 @@ public class ORTWorld
 {
     public static List<string> ListaDestinos {get; private set;} = new List<string>
     {
-        "buenos aires", "paris", "barcelona",  "canada",  "florida"/*🦅🦅🦅*/,  "polonia",  "lituania",  "senegal",  "grieta del invocador",  "runaterra"
+        "buenos aires", "paris", "barcelona",  "ottawa",  "florida"/*🦅🦅🦅*/,  "varsovia",  "vilna",  "dakar",  "grieta del invocador",  "runaterra"
     };
     public static List<string> ListaHoteles {get; private set;} = new List<string>
     {
-        "trivago", "santa maria", "babilonia", "stanca", "bikel", "momazos", "grasa", "notengoideadequeponer", "goku", "kratos" 
+        "trivago", "santa maria", "babilonia", "stanca", "cherniak", "momazos", "grasa", "notengoideadequeponer", "goku", "kratos" 
     };
     public static List<string> ListaAereos {get; private set;} = new List<string>
     {
-        "stanca", "binker", "iberia", "poppy"/*Aerolineas Martillo*/, "diegollanos", "shebar", "readme.md", "aereo", "aprobanosporfa", "quemalmomo", 
+        "stanca", "binker", "iberia", "poppy"/*Aerolineas Martillo*/, "maradona", "aerolineas argentinas", "readme.md", "aereo", "aprobanosporfa", "india", 
     };
     public static List<string> ListaExcursiones {get; private set;} = new List<string>
     {
